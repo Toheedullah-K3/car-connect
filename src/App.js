f1cda375f9ee52a6d850ded1083b20d9a4d2f496
Toheedullah-K3 <toheedullah002@gmail.com>
2024-04-01 01:03:55 +0500

    commiting...

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 function App() {
   return (
-    <>
+    <div className="App">
       <h1> Project Title: Car Connect </h1>
       <h2> Project Members:- </h2>
       <h3> Bilal Asghar - 4434-FBAS/BSCS4/F21 </h3>
       <h3> Toqeer Zia - 4455-FBAS/BSCS4/F21 </h3>
+      <h3> Abdullah Iftikhar - 4443-FBAS/BSCS4/F21 </h3>
       <h3> Toheed Ullah Khan - 4453-FBAS/BSCS4/F21 </h3>
-    </>
+    </div>
   );
 }
 
