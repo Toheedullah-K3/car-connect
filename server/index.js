43f8061a12e807259828b38de02f7373c02bb981
Toheedullah-K3 <toheedullah002@gmail.com>
2024-05-19 02:22:06 +0500

    first commit

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
 const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
 const app = express()
-const port = 3000
+const port = process.env.PORT
+
+app.use(express.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get('/login', (req, res) => {
+  res.send('Hello 33!')
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
